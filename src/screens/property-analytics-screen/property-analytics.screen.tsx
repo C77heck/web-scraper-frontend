307cd9a993565b0006fc32b5250354d9d051c828
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2023-02-23 19:13:37 +0100

    wip

--- src/screens/property-analytics-screen/property-analytics.screen.tsx
+++ src/screens/property-analytics-screen/property-analytics.screen.tsx
@@ -3,8 +3,15 @@ import { useClient } from '../../shared/hooks/client.hook';
 import { ScreenProps, ScreenRoute } from '../libs/screen.route';
 import { SqmPriceChart } from './components/sqm-price.chart';
 
+export interface AnalyticsData {
+    sqmPrices: number[];
+    dateOn: number[];
+    totalPrices: number[];
+    sizes: number[];
+}
+
 export const PropertyAnalyticsScreen = (props: ScreenProps) => {
-    const { get, data, loading } = useClient<{ dataset: number[] }>();
+    const { get, data, loading } = useClient<AnalyticsData>();
 
     useEffect(() => {
         (async () => get({ url: '/analytics/kecskemet/flat' }))();
@@ -13,7 +20,7 @@ export const PropertyAnalyticsScreen = (props: ScreenProps) => {
     return <ScreenRoute {...props}>
         <div className={'row'}>
             <div className={'col-md-50 col-100'}>
-                <SqmPriceChart dataset={data?.dataset || []} loading={loading}/>
+                <SqmPriceChart dataset={data?.sqmPrices || []} loading={loading}/>
             </div>
             <div className={'col-md-50 col-100'}>
 
